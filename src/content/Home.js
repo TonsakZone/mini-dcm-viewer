e0241a2a26315779722ab6b379c3021a1182a688
Supphakorn Innuphat <91209210+TonsakZone@users.noreply.github.com>
2021-11-16 19:13:08 +0700

    Remove moving shapes

--- src/content/Home.js
+++ src/content/Home.js
@@ -10,22 +10,22 @@ function Home() {
             <a href="https://www.kaggle.com/c/siim-covid19-detection/data" target="_blank" rel="noreferrer">DCM image and CSV file from Kaggle</a>
             <br/>
             <a href="https://www.youtube.com/watch?v=qx7pSLjLNQA" target="_blank" rel="noreferrer">Extra: Animate background using CSS</a>
-//             <div className="box">
-//                 <div></div>
-//                 <div></div>
-//                 <div></div>
-//                 <div></div>
-//             </div>
-//             <div className="triangle">
-//                 <div></div>
-//                 <div></div>
-//                 <div></div>
-//                 <div></div>
-//             </div>
-//             <div className="circle">
-//                 <div></div>
-//                 <div></div>
-//             </div>
+          {/* <div className="box">
+                <div></div>
+                <div></div>
+                 <div></div>
+                <div></div>
+           </div>
+           <div className="triangle">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+             <div className="circle">
+                 <div></div>
+                <div></div>
+            </div> */}
         </div>
     )
 }
